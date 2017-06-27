9d1fb8edab801d797441b39a5ea0bee1953347d0
Jörg Domaschka <joerg.domaschka@uni-ulm.de>
2017-06-27 15:07:28 +0200

    - added missing module. - add eclipse excludes to gitignore

--- orchestration/src/main/java/io/github/cloudiator/virtualmachine/VirtualMachineAgent.java
+++ orchestration/src/main/java/io/github/cloudiator/virtualmachine/VirtualMachineAgent.java
@@ -4,13 +4,16 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 import org.cloudiator.messaging.kafka.KafkaMessagingModule;
+import org.cloudiator.messaging.services.MessageServiceModule;
 
 public class VirtualMachineAgent {
 
-  private static Injector injector = Guice.createInjector(new KafkaMessagingModule());
+  private static Injector injector =
+      Guice.createInjector(new KafkaMessagingModule(), new MessageServiceModule());
 
-  /** 
+  /**
    * starts the virtual machine agent.
+   * 
    * @param args args
    */
   public static void main(String[] args) {
