47214a9d4fb3a77dd14fdf03200e50cf44d4302c
Daniel Baur <daniel.baur@uni-ulm.de>
2017-07-19 10:46:39 +0200

    Removed old code

--- orchestration/src/main/java/io/github/cloudiator/KeyPairRepository.java
+++
@@ -1,8 +0,0 @@
-package io.github.cloudiator;
-
-/**
- * Created by daniel on 29.06.17.
- */
-public class KeyPairRegistry {
-
-}
--- orchestration/src/main/java/io/github/cloudiator/workflow/CreateKeyPairForVM.java
+++
@@ -1,8 +0,0 @@
-package io.github.cloudiator.workflow;
-
-/**
- * Created by daniel on 29.06.17.
- */
-public class CreateKeyPairForVM {
-
-}
