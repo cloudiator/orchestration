31e50a4838b53012adff1088c37fdcd024dd42d7
Daniel Seybold <daniel.seybold@uni-ulm.de>
2017-07-20 09:21:09 +0200

    added snap to unix installer

--- installer/src/main/java/org/cloudiator/orchestration/installer/tools/installer/UnixInstaller.java
+++ installer/src/main/java/org/cloudiator/orchestration/installer/tools/installer/UnixInstaller.java
@@ -220,6 +220,8 @@ public class UnixInstaller extends AbstractInstaller {
         this.installKairosDb();
 
         this.installVisor();
+
+        this.installSnap();
         
         this.installSnap();
     }
