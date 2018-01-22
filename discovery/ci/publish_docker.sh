ee91fbe9b417bc8ad6f72d4c451c546844deff1c
Daniel Baur <daniel.baur@uni-ulm.de>
2018-01-22 17:03:31 +0100

    Fixed docker build

--- discovery/ci/publish_docker.sh
+++ discovery/ci/publish_docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 export REPO=cloudiator/discovery
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
-docker build -f Dockerfile -t $REPO:$COMMIT discovery
+docker build -t $REPO:$COMMIT discovery
 docker tag $REPO:$COMMIT $REPO:$TAG
 docker tag $REPO:$COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
 docker push $REPO
