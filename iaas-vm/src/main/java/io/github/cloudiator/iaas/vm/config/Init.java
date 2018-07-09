d585eeb44ce0ed517461444531159053fae3a556
Daniel Baur <daniel.baur@uni-ulm.de>
2018-07-09 17:09:34 +0200

    Added initial restore of cloud registry

--- iaas-vm/src/main/java/io/github/cloudiator/iaas/vm/config/Init.java
+++ iaas-vm/src/main/java/io/github/cloudiator/iaas/vm/config/Init.java
@@ -2,9 +2,16 @@ package io.github.cloudiator.iaas.vm.config;
 
 import com.google.inject.Inject;
 import com.google.inject.persist.PersistService;
+import de.uniulm.omi.cloudiator.sword.domain.Cloud;
 import de.uniulm.omi.cloudiator.sword.multicloud.service.CloudRegistry;
 import io.github.cloudiator.messaging.CloudMessageRepository;
 import io.github.cloudiator.persistance.TenantDomainRepository;
+import java.util.stream.Collectors;
+import org.cloudiator.messages.entities.User.TenantQueryRequest;
+import org.cloudiator.messages.entities.User.TenantQueryResponse;
+import org.cloudiator.messages.entities.UserEntities.Tenant;
+import org.cloudiator.messaging.ResponseException;
+import org.cloudiator.messaging.services.UserService;
 
 /**
  * Created by daniel on 31.05.17.
@@ -15,16 +22,18 @@ class Init {
   private final CloudMessageRepository cloudMessageRepository;
   private final TenantDomainRepository tenantDomainRepository;
   private final CloudRegistry cloudRegistry;
+  private final UserService userService;
 
   @Inject
   Init(PersistService persistService,
       CloudMessageRepository cloudMessageRepository,
       TenantDomainRepository tenantDomainRepository,
-      CloudRegistry cloudRegistry) {
+      CloudRegistry cloudRegistry, UserService userService) {
     this.persistService = persistService;
     this.cloudMessageRepository = cloudMessageRepository;
     this.tenantDomainRepository = tenantDomainRepository;
     this.cloudRegistry = cloudRegistry;
+    this.userService = userService;
     run();
   }
 
@@ -38,12 +47,25 @@ class Init {
   }
 
   private void restoreCloudRegistry() {
-    for (String tenant : tenantDomainRepository.tenants()) {
-      cloudMessageRepository.getAll(tenant).forEach(cloud -> {
-        if (!cloudRegistry.isRegistered(cloud)) {
-          cloudRegistry.register(cloud);
+
+    try {
+      final TenantQueryResponse tenants = userService
+          .getTenants(TenantQueryRequest.newBuilder().build());
+
+      for (String tenant : tenants.getTenantList().stream().map(Tenant::getTenant)
+          .collect(
+              Collectors
+                  .toSet())) {
+        for (Cloud cloud : cloudMessageRepository.getAll(tenant)) {
+          if (!cloudRegistry.isRegistered(cloud)) {
+            cloudRegistry.register(cloud);
+          }
         }
-      });
+      }
+
+
+    } catch (ResponseException e) {
+      throw new IllegalStateException("Could not restore initial state of cloud registry");
     }
   }
 
