899961f7a34a4d74352097d8d1e1085360e8bb96
Daniel Baur <daniel.baur@uni-ulm.de>
2018-10-02 10:56:47 +0200

    Refactoring to match new messages

--- iaas-common/src/main/java/io/github/cloudiator/messaging/ConfigurationMessageToConfiguration.java
+++ iaas-common/src/main/java/io/github/cloudiator/messaging/ConfigurationMessageToConfiguration.java
@@ -4,7 +4,6 @@ import de.uniulm.omi.cloudiator.sword.domain.Configuration;
 import de.uniulm.omi.cloudiator.sword.domain.ConfigurationBuilder;
 import de.uniulm.omi.cloudiator.sword.domain.PropertiesBuilder;
 import de.uniulm.omi.cloudiator.util.TwoWayConverter;
-import java.util.function.BiConsumer;
 import org.cloudiator.messages.entities.IaasEntities;
 import org.cloudiator.messages.entities.IaasEntities.Configuration.Builder;
 
@@ -22,8 +21,8 @@ class ConfigurationMessageToConfiguration
   @Override
   public Configuration apply(IaasEntities.Configuration configuration) {
     final PropertiesBuilder propertiesBuilder = PropertiesBuilder.newBuilder();
-    configuration.getPropertyList().forEach(
-        property -> propertiesBuilder.putProperty(property.getKey(), property.getValue()));
+    configuration.getPropertiesMap()
+        .forEach(propertiesBuilder::putProperty);
     return ConfigurationBuilder.newBuilder().nodeGroup(configuration.getNodeGroup())
         .properties(propertiesBuilder.build()).build();
   }
@@ -31,12 +30,8 @@ class ConfigurationMessageToConfiguration
   @Override
   public IaasEntities.Configuration applyBack(Configuration configuration) {
     final Builder builder = IaasEntities.Configuration.newBuilder();
-    configuration.properties().getProperties().forEach(new BiConsumer<String, String>() {
-      @Override
-      public void accept(String key, String value) {
-        builder.addPropertyBuilder().setKey(key).setValue(value);
-      }
-    });
+    configuration.properties().getProperties().forEach(
+        builder::putProperties);
     builder.setNodeGroup(configuration.nodeGroup());
     return builder.build();
   }
