0b842600ccc39fc6463fe106ea344bfe246c5149
Daniel Seybold <daniel.seybold@uni-ulm.de>
2019-05-09 14:52:51 +0200

    trying to fix sonar cube secure vars solution

--- ci/script.sh
+++ ci/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$TRAVIS_SECURE_ENV_VARS" = true ]; then
+if [ "${SONAR_SCANNER_HOME}" != "" ]; then
    mvn -q org.jacoco:jacoco-maven-plugin:prepare-agent install sonar:sonar -Dmaven.javadoc.skip=true
 else
   mvn -q org.jacoco:jacoco-maven-plugin:prepare-agent install -Dmaven.javadoc.skip=true
