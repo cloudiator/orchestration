0d38c4aab521a5dda0f559382da5c02369cea5dc
Daniel Baur <daniel.baur@uni-ulm.de>
2019-06-14 14:41:38 +0200

    virtual machine watchdog now requires to iterations to mark a vm as failed

--- iaas-vm/src/main/java/io/github/cloudiator/iaas/vm/watchdog/VirtualMachineErrorWatchdog.java
+++ iaas-vm/src/main/java/io/github/cloudiator/iaas/vm/watchdog/VirtualMachineErrorWatchdog.java
@@ -20,6 +20,7 @@ package io.github.cloudiator.iaas.vm.watchdog;
 
 import com.google.common.base.MoreObjects;
 import com.google.inject.Inject;
+import de.uniulm.omi.cloudiator.domain.Identifiable;
 import de.uniulm.omi.cloudiator.sword.domain.VirtualMachine;
 import de.uniulm.omi.cloudiator.sword.service.ComputeService;
 import de.uniulm.omi.cloudiator.util.execution.Schedulable;
@@ -27,8 +28,11 @@ import io.github.cloudiator.domain.ExtendedVirtualMachine;
 import io.github.cloudiator.domain.LocalVirtualMachineState;
 import io.github.cloudiator.iaas.vm.state.VirtualMachineStateMachine;
 import io.github.cloudiator.persistance.VirtualMachineDomainRepository;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -40,6 +44,8 @@ public class VirtualMachineErrorWatchdog implements Schedulable {
   private final ComputeService computeService;
   private final VirtualMachineDomainRepository virtualMachineDomainRepository;
   private final VirtualMachineStateMachine virtualMachineStateMachine;
+
+  private static Set<String> FAILED_IN_LAST_ITERATION = new HashSet<>();
 
   @Inject
   public VirtualMachineErrorWatchdog(
@@ -71,27 +77,42 @@ public class VirtualMachineErrorWatchdog implements Schedulable {
 
     LOGGER.info(String.format("%s is running", this));
 
-    final Iterable<VirtualMachine> virtualMachines = computeService.discoveryService()
+    final Iterable<VirtualMachine> remoteVirtualMachines = computeService.discoveryService()
         .listVirtualMachines();
 
-    for (ExtendedVirtualMachine extendedVirtualMachine : virtualMachineDomainRepository.findAll()
+    LOGGER.debug("Remotely existing virtual machines: " + StreamSupport
+        .stream(remoteVirtualMachines.spliterator(), false).map(Identifiable::id)
+        .collect(Collectors.toSet()));
+
+    for (ExtendedVirtualMachine localVM : virtualMachineDomainRepository.findAll()
         .stream().filter(
             extendedVirtualMachine -> extendedVirtualMachine.state()
                 .equals(LocalVirtualMachineState.RUNNING)).collect(
             Collectors.toSet())) {
 
       LOGGER.debug(
-          String.format("Checking if virtual machine %s still exists", extendedVirtualMachine));
+          String.format("Checking if virtual machine with id %s still exists",
+              localVM.id()));
 
-      final boolean stillExists = checkIfStillExists(extendedVirtualMachine, virtualMachines);
+      final boolean stillExists = checkIfStillExists(localVM, remoteVirtualMachines);
 
       if (!stillExists) {
-        LOGGER.warn(String.format("Virtual machine %s does not longer exist, marking failed",
-            extendedVirtualMachine));
-
-        virtualMachineStateMachine.fail(extendedVirtualMachine, new Object[0], null);
+        if (FAILED_IN_LAST_ITERATION.contains(localVM.id())) {
+          LOGGER
+              .warn(String.format("Virtual machine %s does not longer exist, marking it as failed.",
+                  localVM));
+          FAILED_IN_LAST_ITERATION.remove(localVM.id());
+          virtualMachineStateMachine.fail(localVM, new Object[0], null);
+        } else {
+          LOGGER.warn(String.format(
+              "Virtual machine %s failed for the first time. Checking again at the next iteration.",
+              localVM));
+          FAILED_IN_LAST_ITERATION.add(localVM.id());
+        }
       } else {
-        LOGGER.debug(String.format("Virtual machine %s still exists.", extendedVirtualMachine));
+        LOGGER.debug(
+            String.format("Virtual machine with id %s still exists.", localVM.id()));
+        FAILED_IN_LAST_ITERATION.remove(localVM.id());
       }
     }
 
